389d1dd84d9bfac7ddeb6e3f9a20aca8810f0442
xaitx <admin@xaitx.com>
2024-11-15 18:05:26 +0800

    代理测试文件

--- dirscan/dirscan_test.go
+++ dirscan/dirscan_test.go
@@ -1,6 +1,7 @@
 package dirscan_test
 
 import (
+	"io"
 	"testing"
 
 	"github.com/xaitx/dirscan/dirscan"
@@ -17,9 +18,27 @@ func TestReadDictFile(t *testing.T) {
 
 // 测试请求函数
 func TestRequest(t *testing.T) {
-	response, err := dirscan.Request("http://www.baidu.com", "GET", nil, "")
+	logs.PrintColor(logs.Blue, "不使用代理请求")
+	response, err := dirscan.Request("https://ifconfig.me/ip", "GET", nil, "")
 	if err != nil {
 		t.Error(err)
 	}
-	logs.Print(response)
+	// 读取响应内容
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	logs.Info(string(body))
+
+	logs.PrintColor(logs.Blue, "使用代理请求")
+	response, err = dirscan.Request("https://ifconfig.me/ip", "GET", nil, "socks5://127.0.0.1:7890")
+	if err != nil {
+		t.Error(err)
+	}
+	// 读取响应内容
+	body, err = io.ReadAll(response.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	logs.Info(string(body))
 }
